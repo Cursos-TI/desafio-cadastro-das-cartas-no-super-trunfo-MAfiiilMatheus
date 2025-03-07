6589af8034a3a358f255cb0a1ffe48ce99b7fbe9
Matheus Santos Oliveira <202502620888@alunos.estacio.br>
2025-03-07 22:33:19 +0000

    criando a primeira Carta

---
+++ Desafio Super Trunfo/Carta 01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main (){
+
+    char estado [50] ;
+    char codigo [50];
+    char cidade [50];
+    int populacao ;
+    float area ;
+    float PIB ;
+    int pontos ;
+
+    printf ("Digite o Estado: \n");
+    scanf ("%s", &estado);
+    printf ("Estado: \n %s");
+    return 0;
+
+    
+
+
+
+
+
+
+}
